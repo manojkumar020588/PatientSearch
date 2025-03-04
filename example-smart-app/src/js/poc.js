484e3f9b36f23391a82e470262aeb20fe282d0ea
manojkumar020588 <42073556+manojkumar020588@users.noreply.github.com>
2025-03-04 11:31:07 +0000

    Update poc.js

--- example-smart-app/src/js/poc.js
+++ example-smart-app/src/js/poc.js
@@ -30,7 +30,7 @@
              
                 const getPath = FHIR.client("https://r2.smarthealthit.org").getPath;
                 FHIR.client("https://r2.smarthealthit.org")
-                .request("/Medication?id=12724067", {
+                .request("/Medication?_id=12724067", {
                     resolveReferences: [ "medicationReference" ],
                     graph: true
                 }).then(data => data.entry.map(item => getMedicationName(
